22d9d0180af63d6d82885d8e0e6adadfedd8cbfc
baymerid <bay.merdan@gmail.com>
2022-01-04 14:12:35 +0500

    3.2 creating tabs

---
+++ js/script.js
@@ -0,0 +1,36 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    'use strict';
+
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideContent (a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideContent(1);
+
+    function showContent (b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+        
+    }
+    
+    info.addEventListener('click', (event)=> {
+        if (event.target && event.target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (event.target == tab[i]) {
+                    hideContent(0);
+                    showContent(i);
+                }
+            }        
+        }
+    })
+})
